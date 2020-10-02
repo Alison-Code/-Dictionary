a5f843f61f61cd83ee6094313178d15c711450e7
Pomo-granade <72248168+Pomo-granade@users.noreply.github.com>
2020-10-02 14:05:20 +0530

    Update exercise 1 APNI DICTIONARY.py

--- exercise 1 APNI DICTIONARY.py
+++ exercise 1 APNI DICTIONARY.py
@@ -1,8 +1,16 @@
-prete":",
-    "amcable":"friedly",
-    "noel":"chistas",
-    "formidable":"inmidating"
+print("\nWELCOME TO MY DICTIONARY")
 
+d={"seethe":"boil",
+    "amicable":"friendly",
+    "noel":"christmas",
+    "formidable":"intimidating"
+}
 print("WORDS -> seethe , amicable , noel , formidable")
-print("Chose your woand write here to know the meaning")
+print("Choose your word and write here to know the meaning")
 wrd = input()
+
+if wrd not in d:
+    print("THE WORD IS NOT IN THE DICTIONARY")
+
+else:
+    print("The meaning of the word is", d[wrd])
